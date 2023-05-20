26ee22bab887bd51d1fcd00fe9a20f5e1e1e2a4c
ignizxl <igor.joao@academico.ifpb.edu.br>
2023-05-19 23:10:52 -0300

    update

---
+++ poo06/src/main/java/com/mycompany/poo06/Livro.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo06;
+
+/**
+ *
+ * @author João Igor
+ */
+public class Livro implements Publicacao{
+    private String titulo;
+    private String autor;
+    private int totalDePaginas; 
+    private int paginaAtual;
+    private boolean aberto;
+    private Pessoa leitor;
+
+    public Livro(String titulo, String autor, int totalDePaginas, Pessoa leitor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.totalDePaginas = totalDePaginas;
+        this.leitor = leitor;
+  
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getTotalDePaginas() {
+        return totalDePaginas;
+    }
+
+    public void setTotalDePaginas(int totalDePaginas) {
+        this.totalDePaginas = totalDePaginas;
+    }
+
+    public int getPaginaAtual() {
+        return paginaAtual;
+    }
+
+    public void setPaginaAtual(int paginaAtual) {
+        this.paginaAtual = paginaAtual;
+    }
+
+    public boolean getAberto() {
+        return aberto;
+    }
+
+    public void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+    public Pessoa getLeitor() {
+        return leitor;
+    }
+
+    public void setLeitor(Pessoa leitor) {
+        this.leitor = leitor;
+    }
+
+    @Override
+    public void abrir() {
+        this.setAberto(true);
+    }
+
+    @Override
+    public void fechar() {
+        this.setAberto(false);
+    }
+
+    @Override
+    public void folhear(int pagina) {
+        this.setPaginaAtual(pagina);
+        
+    }
+
+    @Override
+    public void avancarPagina() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void voltarPagina() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
+    
+    
---
+++ poo06/src/main/java/com/mycompany/poo06/Publicacao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo06;
+
+/**
+ *
+ * @author João Igor
+ */
+public interface Publicacao {
+    public void abrir();
+    public void fechar();
+    public void folhear();
+    public void avancarPagina();
+    public void voltarPagina();
+    
+}
